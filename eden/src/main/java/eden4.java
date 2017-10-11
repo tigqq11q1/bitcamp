14abfbba9adb4d0b2158f7f758262a2489314537
tigqq11q1 <tigqq11q1@naver.com>
2017-10-11 10:30:36 +0900

    s

---
+++ eden/src/main/java/eden4.java
@@ -0,0 +1,21 @@
+package bitcamp.java100;
+
+public class eden4{
+    public static void main(String[]args){
+        //int a;
+        int[]score = new int[]{100,200,300,400,500};
+       // int[]eden = {10,20,30,40,50,};
+       // a = eden[2];
+        
+        for(int i =0; i < score.length; i++){
+          
+                System.out.printf("%d %d %d %d %d",score[i]);
+             
+            
+        }
+        }
+    }
+
+
+
+
