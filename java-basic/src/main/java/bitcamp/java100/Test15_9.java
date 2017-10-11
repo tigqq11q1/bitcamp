5fbda5bb7e2a5da6152854b2f72de3b96080ee2a
eden <tigqq11q1@naver.com>
2017-10-11 11:39:59 +0900

    dd

---
+++ java-basic/src/main/java/bitcamp/java100/Test15_9.java
@@ -0,0 +1,19 @@
+package bitcamp.java100;
+
+public class Test15_9{
+        public static void main(String[] args) {
+            int age = 20;
+            
+            String result = (age <= 19) ? "미성년" : "성년";           
+            
+            System.out.println(result);
+           
+  
+    
+                
+                
+     }
+}
+
+
+
