5cac578d2f6f5dc4ba0d8899c3c4de9655821e26
tigqq11q1 <tigqq11q1@naver.com>
2017-10-11 10:30:04 +0900

    ...

---
+++ eden/src/main/java/eden10.java
@@ -0,0 +1,21 @@
+package bitcamp.java100;
+
+public class eden10{
+    public static void main(String[]args){
+     int[][] eden = new int[2][2];
+     eden[0][0] = 20;
+     eden[0][1] = 30;
+     eden[1][0] = 40;
+     eden[1][1] = 60;
+     
+     System.out.printf("%d %d",eden[0][1],eden[1][1]);
+
+    }
+}
+
+        
+    
+    
+
+
+
