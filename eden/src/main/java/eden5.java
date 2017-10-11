3e1f01d304d50ace5590595dc4610220459ee726
tigqq11q1 <tigqq11q1@naver.com>
2017-10-11 10:36:50 +0900

    s

---
+++ eden/src/main/java/eden5.java
@@ -0,0 +1,29 @@
+package bitcamp.java100;
+
+public class eden5{
+    public static void main(String[]args){
+        int a;
+        int b;
+        int c;
+        
+        class Score{
+            String name;
+            int age;
+            int math;
+            int eng;
+        }
+        Score r1 = new Score();
+
+        r1.name = "김이든";
+        r1.age = 25;
+        r1.math = 80;
+        r1.eng = 100;
+        a = 30;
+        
+        System.out.printf("%s %d %d %d %d\n",r1.name,a, r1.age, r1.math, r1.eng);
+        }
+    }
+
+
+
+
